c7192762957e8e92b5569ba1f412f766f30eb36d
Yauheni Bandyukevich <zhenya.bandyukevich@mail.ru>
2020-02-24 00:45:01 +0300

    feat: add solution

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,11 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
-};
+module.exports = function countCats(array) {
+  let result = 0;
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array[i].length; j++) {
+      if (array[i][j] == "^^") {
+        result++;
+      }
+    }
+  }
+  return result;
+}
