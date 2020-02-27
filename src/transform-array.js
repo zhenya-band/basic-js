1d16f3eb25b588210ead14e9bb791ee65da1d12f
Yauheni Bandyukevich <zhenya.bandyukevich@mail.ru>
2020-02-27 22:42:04 +0300

    feat: solution "what-season"

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,17 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    for (var i = 0; i < arr.length; i++) {
+      if (true) {
+
+      }
+
+    }
+
+
+
+    /*
+    --discard-next excludes the next element of the array from the transformed array.
+    --discard-prev excludes the previous element of the array from the transformed array.
+    --double-next doubles the next element of the array in the transformed array.
+    --double-prev doubles the previous element of the array in the transformed array.
+    */
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,15 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  //Errors
+  if (!date) {
+    return 'Unable to determine the time of year!';
+  }
+  let toString = ({}.toString);
+  if((toString.call(date)) !== '[object Date]'){
+     throw new Error('Error');
+  }
+  // main
+  let month = date.getMonth();
+  const season = ['winter', 'winter', 'spring', 'spring', 'spring', 'summer', 'summer', 'summer', 'autumn', 'autumn', 'autumn','winter'];
+  return season[month];
+
 };
