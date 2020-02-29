4031111f5eeb79a84bc4766921d544c0d6a90802
Yauheni Bandyukevich <zhenya.bandyukevich@mail.ru>
2020-03-01 02:01:35 +0300

    feat: carbon-dating solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,13 @@
-const MODERN_ACTIVITY= 15; 
+const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
-
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if (typeof(sampleActivity) == 'string') {
+    sampleActivity = parseFloat(sampleActivity);
+  }
+  else return false;
+  let k = 0.693 / HALF_LIFE_PERIOD;
+  if (sampleActivity > 0 && sampleActivity < 15) {
+    return Math.ceil((Math.log(MODERN_ACTIVITY / sampleActivity) / k));
+  }
+  else return false;
 };
