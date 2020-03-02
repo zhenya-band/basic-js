fad20673e1ce3f101c19fad2137aea4750c975b9
Yauheni Bandyukevich <zhenya.bandyukevich@mail.ru>
2020-03-02 23:18:38 +0300

    feat: recursive-depth solution

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,6 +1,13 @@
-module.exports = class DepthCalculator {
-    calculateDepth(/* arr */) {
-        throw 'Not implemented';
-        // remove line with error and write your code here
-    }
-};
+
+  let max_depth = 0;
+  module.exports = class DepthCalculator {
+    calculateDepth(arr, current_depth = 1) {
+      for (let i = 0; i < arr.length; i++) {
+          if(Array.isArray(arr[i])) max_depth = this.calculateDepth(arr[i], current_depth + 1);
+      }
+      max_depth = (max_depth > current_depth) ? max_depth : current_depth;
+      let depth = max_depth;
+      max_depth = 0;
+      return depth;
+  }
+}
