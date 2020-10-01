c8bc62b0130deb313ebaf437319adf07b03ae3e0
Yauheni Bandyukevich <zhenya.bandyukevich@mail.ru>
2020-10-02 01:59:55 +0300

    feat: add simple-chain solution

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,23 +1,32 @@
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    return this.chain.length;
   },
   addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if (value !== '') {
+      this.chain.push(`( ${value} )`);
+    } else {
+      this.chain.push('( )')
+    }
+    return this;
   },
   removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if (typeof(position) !== 'number' || position < 1 || position >= this.chain.length || !Number.isInteger(position) || isNaN(position)) {
+      this.chain = [];
+      throw new Error();
+    }
+    this.chain.splice(position - 1, 1);
+    return this;
   },
   reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.chain = this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    const result = this.chain.join('~~').slice();
+    this.chain = [];
+    return result;
   }
 };
 
