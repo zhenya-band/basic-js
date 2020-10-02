4f6d75b75fe008f63618adc54c0e28a917549c1c
Yauheni Bandyukevich <zhenya.bandyukevich@mail.ru>
2020-10-02 14:33:46 +0300

    feat: add extended-repeater solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,45 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-};
-  
+module.exports = function repeater(str, options) {
+  const {
+    repeatTimes,
+    separator = '+',
+    addition,
+    additionRepeatTimes,
+    additionSeparator = '|'
+  } = options;
+  str = str + '';
+  let additionToString = addition  + '';
+
+  let strAdition = '';
+  let result = '';
+
+  if (additionToString) {
+    if (addition !== undefined && addition !== null) {
+      addition.toString();
+    }
+    for (let i = 0; i < additionRepeatTimes; i++) {
+      if (i === additionRepeatTimes - 1) {
+        strAdition += addition + separator;
+      } else {
+        strAdition += addition + additionSeparator;
+      }
+    }
+  }
+
+  if (strAdition) {
+    str += strAdition;
+  } else if (repeatTimes === undefined) {
+    str += '';
+  } else {
+    str += separator;
+  }
+
+  if (repeatTimes) {
+    for (let i = 0; i < repeatTimes; i++) {
+      result += str;
+    }
+  } else {
+    result += str + addition;
+    return result;
+  }
+  return result.slice(0, -(separator.length));
+}
