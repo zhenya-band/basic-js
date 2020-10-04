cd1cf25ec037ffb20c0f84bbc0f9dcec3fd77b1b
Yauheni Bandyukevich <zhenya.bandyukevich@mail.ru>
2020-10-04 22:55:25 +0300

    fix: fix transform-array solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,30 +1,20 @@
-
-module.exports = function transform(arr){
+module.exports = function transform(arr) {
   if (!(Array.isArray(arr))) throw new Error();
-  let newarr = [];
-    for (let i = 0; i < arr.length; i++) {
-      if(arr[i] != '--discard-prev' && arr[i] != '--double-prev' && arr[i] != '--double-next' && arr[i] != '--discard-next') {
-        newarr.push(arr[i]);
-      }
-      else if (arr[i] == '--discard-next') {
-        i++;
-      }
-      else if (arr[i] == '--discard-prev') {
-        newarr.pop();
-      }
-      else if (arr[i] == '--double-next' && i < arr.length - 1) {
-        newarr.push(arr[i + 1]);
-      }
-      else if (arr[i] == '--double-prev'  && i > 0) {
-        newarr.push(arr[i - 1]);
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] != '--discard-prev' && arr[i] != '--double-prev' && arr[i] != '--double-next' && arr[i] != '--discard-next') {
+      newArr.push(arr[i]);
+    } else if (arr[i] == '--discard-next') {
+      i++;
+    } else if (arr[i] == '--discard-prev' && arr[i - 2] !== '--discard-next') {
+      newArr.pop();
+    } else if (arr[i] == '--double-next' && i < arr.length - 1) {
+      newArr.push(arr[i + 1]);
+    } else if (arr[i] == '--double-prev' && i > 0) {
+      if (arr[i - 2] !== '--discard-next') {
+        newArr.push(arr[i - 1]);
       }
     }
-
-    /*
-    --discard-next excludes the next element of the array from the transformed array.
-    --discard-prev excludes the previous element of the array from the transformed array.
-    --double-next doubles the next element of the array in the transformed array.
-    --double-prev doubles the previous element of the array in the transformed array.
-    */
-      return newarr;
+  }
+  return newArr;
 };
